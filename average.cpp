ebc681008c998c84499dd011f4e7b6c4fd988e0b
Me-i2004 <rapeephan_ch@cmu.ac.th>
2026-02-25 17:49:55 +0700

    lan21.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int main(int argc,char *argv[]){
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }
+    else{
+        int count = argc-1;
+        double sum = 0.0;
+        
+        for(int i=1;i<argc;i++){
+            sum += atof(argv[i]);
+        }
+        
+        cout << "---------------------------------\n";
+        cout << "Average of " << count << " numbers = " << sum/count << "\n";
+        cout << "---------------------------------";
+    }
+return 0;
+}
